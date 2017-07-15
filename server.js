124dec7940a13468c6873b70b0435f0bd99d327c
JaspherJ <jazoswald@gmail.com>
2017-07-14 19:55:10 -0500

    server

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var app = express();
 const hbs = require("hbs");
 const MongoClient =require("mongodb").MongoClient;
 
-
 MongoClient.connect('mongodb://localhost:27017/test',(err,db)=>{
     if(err){
        return  console.log("Unable to connect");
